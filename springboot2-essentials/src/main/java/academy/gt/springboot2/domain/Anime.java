e9f3638e1a77a7796bf67198d96a6ec91c945f7a
Giovani Trevisol <giovanitrevisol93@gmail.com>
2021-01-25 23:33:31 -0300

    first commit

---
+++ springboot2-essentials/src/main/java/academy/gt/springboot2/domain/Anime.java
@@ -0,0 +1,19 @@
+package academy.gt.springboot2.domain;
+
+public class Anime {
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+public Anime(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
