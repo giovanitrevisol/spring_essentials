5540020b0b2f8bf2d7916b1afebae8016acbfb90
Giovani Trevisol <giovanitrevisol93@gmail.com>
2021-01-27 23:07:02 -0300

    refactory

--- springboot2-essentials/curso/curso/src/main/java/com/gt/curso/CursoSbApplication.java
+++ springboot2-essentials/curso/curso/src/main/java/com/gt/curso/CursoSbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursoSbDevdojoApplication {
+public class CursoSbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CursoSbDevdojoApplication.class, args);
+		SpringApplication.run(CursoSbApplication.class, args);
 	}
 
 }
